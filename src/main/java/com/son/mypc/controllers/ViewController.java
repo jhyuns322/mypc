7927cc164c6033c85ad1cb88700d4faa61d93e38
jonghyun <jhyuns322@gmail.com>
2020-03-03 04:38:07 +0900

    스케줄러 수정

--- src/main/java/com/son/mypc/controllers/ViewController.java
+++ src/main/java/com/son/mypc/controllers/ViewController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,8 +35,12 @@ import com.son.mypc.service.ItemService;
 import com.son.mypc.service.MemberService;
 import com.son.mypc.service.OrderService;
 import com.son.mypc.service.PayService;
+
+import lombok.extern.slf4j.Slf4j;
+
 import com.google.gson.Gson;
 
+@Slf4j
 @Controller
 public class ViewController {
 
@@ -1047,5 +1052,44 @@ public class ViewController {
         // View를 사용하지 않고 FileStream을 출력하므로 리턴값은 없다.
         return null;
     }
+    
+    /** 기타) 만료 쿠폰 수정 */
+    @Scheduled(cron="0 0 0 * * ?")
+	public void everyMidNight() {
+		
+		List<Coupon> output = null;
+		Coupon input = new Coupon();
+		try {
+			output = couponService.getAllList();
+		} catch (Exception e) {
+			return;
+		}
+		
+		Calendar cal = Calendar.getInstance();
+		
+		int yy = cal.get(Calendar.YEAR);
+		int mm = cal.get(Calendar.MONTH) +1;
+		int dd = cal.get(Calendar.DAY_OF_MONTH);
+		
+		int now_date = Integer.parseInt(String.format("%d%02d%02d", yy,mm,dd));
+		int expired_date = 0;
+		String expired_date_s;
+		System.out.println("현재시각"+now_date);
+		
+		for (int i = 0; i < output.size(); i++) {
+			expired_date_s = output.get(i).getExpired_date().replace("-", "");
+			expired_date = Integer.parseInt(expired_date_s.substring(0, 8));
+			System.out.println(expired_date);
+			if (now_date >= expired_date) {
+				input.setCoupno(output.get(i).getCoupno());
+				try {
+					couponService.editExpiredCoupon(input);
+					log.debug("쿠폰 수정에 성공했습니다.");
+				} catch (Exception e) {
+					return;
+				}
+			}
+		}
+	}
     
 }
